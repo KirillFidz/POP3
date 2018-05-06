21bdab01cf92f43230c966da419381eb874f3a1e
Kirill Fidzykevich <36966618+KirillFidz@users.noreply.github.com>
2018-05-06 12:00:57 +0300

    Add files via upload

---
+++ POP3/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace POP3
+{
+    partial class fmMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.cbCommand = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblError = new System.Windows.Forms.Label();
+            this.tbArg = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnConnect.Location = new System.Drawing.Point(7, 339);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(124, 28);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Подкючиться";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cbCommand
+            // 
+            this.cbCommand.Enabled = false;
+            this.cbCommand.FormattingEnabled = true;
+            this.cbCommand.Items.AddRange(new object[] {
+            "STAT",
+            "LIST",
+            "NOOP",
+            "DELE"});
+            this.cbCommand.Location = new System.Drawing.Point(399, 21);
+            this.cbCommand.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbCommand.Name = "cbCommand";
+            this.cbCommand.Size = new System.Drawing.Size(117, 24);
+            this.cbCommand.TabIndex = 1;
+            this.cbCommand.Text = "STAT";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(141, 127);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(387, 276);
+            this.listBox1.TabIndex = 2;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Enabled = false;
+            this.btnEnter.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnEnter.Location = new System.Drawing.Point(396, 91);
+            this.btnEnter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(120, 28);
+            this.btnEnter.TabIndex = 3;
+            this.btnEnter.Text = "Выполнить";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(104, 25);
+            this.tbLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(249, 22);
+            this.tbLogin.TabIndex = 4;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Location = new System.Drawing.Point(104, 71);
+            this.tbPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.PasswordChar = '*';
+            this.tbPassword.Size = new System.Drawing.Size(249, 22);
+            this.tbPassword.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Логин:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Пароль:";
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.BackColor = System.Drawing.Color.White;
+            this.lblError.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblError.ForeColor = System.Drawing.Color.Black;
+            this.lblError.Location = new System.Drawing.Point(159, 234);
+            this.lblError.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(0, 29);
+            this.lblError.TabIndex = 8;
+            // 
+            // tbArg
+            // 
+            this.tbArg.Enabled = false;
+            this.tbArg.Location = new System.Drawing.Point(399, 53);
+            this.tbArg.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbArg.Name = "tbArg";
+            this.tbArg.Size = new System.Drawing.Size(71, 22);
+            this.tbArg.TabIndex = 9;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Enabled = false;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.Location = new System.Drawing.Point(7, 375);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(124, 28);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // fmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(545, 434);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.tbArg);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.cbCommand);
+            this.Controls.Add(this.btnConnect);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "fmMain";
+            this.Text = "Реализация протокола pop3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cbCommand;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblError;
+        private System.Windows.Forms.TextBox tbArg;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ POP3/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.Security;
+using System.Net.NetworkInformation;
+using System.IO;
+using System.Web;
+
+namespace POP3
+{
+    public partial class fmMain : Form
+    {
+        TcpClient tcpClient;
+        SslStream sslStream;
+        StreamWriter sw;
+        StreamReader reader;
+        string message;
+
+        public fmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                tcpClient = new TcpClient();
+                tcpClient.Connect("pop.mail.ru", 995);
+                if (tcpClient.Connected)
+                {
+                    sslStream = new SslStream(tcpClient.GetStream());
+                    sslStream.AuthenticateAsClient("pop.mail.ru");
+                    sw = new StreamWriter(sslStream);
+                    reader = new StreamReader(sslStream);
+                    if (tbLogin.Text == "" || tbPassword.Text == "")
+                    {
+                        lblError.Text = "Заполните поля <Login> и <Password>";
+                    }
+                    else
+                    {
+                        lblError.Text = "";
+                        sw.WriteLine("USER " + tbLogin.Text);
+                        sw.Flush();
+                        message = reader.ReadLine();
+                        listBox1.Items.Add(message);
+                        sw.WriteLine("PASS " + tbPassword.Text);
+                        sw.Flush();
+                        message = reader.ReadLine();
+                        listBox1.Items.Add(message);
+                        message = reader.ReadLine();
+                        listBox1.Items.Add(message);
+
+                        btnEnter.Enabled = true;
+                        cbCommand.Enabled = true;
+                        tbArg.Enabled = true;
+                        btnExit.Enabled = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblError.Text = ex.Message;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            switch (cbCommand.Text)
+            {
+                case "LIST":
+                    sw.WriteLine(cbCommand.Text + " " + tbArg.Text);
+                    sw.Flush();
+                    message = reader.ReadLine();
+                    listBox1.Items.Add(message);
+                    break;
+                case "DELE":
+                    sw.WriteLine(cbCommand.Text + " " + tbArg.Text);
+                    sw.Flush();
+                    message = reader.ReadLine();
+                    listBox1.Items.Add(message);
+                    break;
+                default:
+                    sw.WriteLine(cbCommand.Text);
+                    sw.Flush();
+                    message = reader.ReadLine();
+                    listBox1.Items.Add(message);
+                    break;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            sw.WriteLine("QUIT");
+            sw.Flush();
+            message = reader.ReadLine();
+            listBox1.Items.Add(message);
+
+            btnEnter.Enabled = false;
+            cbCommand.Enabled = false;
+            tbArg.Enabled = false;
+            btnExit.Enabled = false;
+        }
+    }
+}
